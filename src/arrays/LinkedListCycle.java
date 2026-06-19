ee358a40605f0e619fb450cf18e3fd7d91b1879d
Mallikarjun Kumbar <mallikarjun.kumbar.cs@gmail.com>
2026-06-19 21:47:46 +0530

    Linked List Cycle

---
+++ src/arrays/LinkedListCycle.java
@@ -0,0 +1,46 @@
+package arrays;
+
+public class LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode n1 = new ListNode(3);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(0);
+        ListNode n4 = new ListNode(-4);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n2; // cycle
+
+        System.out.println(hasCycle(n1));
+    }
+}
